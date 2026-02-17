66bf87b2e4f85eab5a02a81f0696e3f83d5c2985
Patrick Brown <brown.patrick@heb.com>
2026-02-17 15:27:45 -0600

    Small changes

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function About() {
             <hr className="separator" />
             <div className="social">
               <p>github: <a href="https://github.com/bicrick" target="_blank" rel="noopener noreferrer">bicrick</a></p>
-              <p>resume: <a href="https://resume.bicrick.com/" target="_blank" rel="noopener noreferrer">link to pdf</a></p>
+              <p>resume: <a href="https://resume.bicrick.com/" target="_blank" rel="noopener noreferrer">pdf</a></p>
               <p>x: <a href="https://x.com/patrickbbrown" target="_blank" rel="noopener noreferrer">patrickbbrown</a></p>
             </div>
           </div>
